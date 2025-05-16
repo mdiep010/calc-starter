8d2cb1ec88f9d9f68c6612f71222a96abf73d8ef
Nam Nguyen <n0mn0m@Nams-Air.attlocal.net>
2025-05-16 13:36:50 -0700

    main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+from operations import add, sub, mult, div
+import sys
+
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: calc <num1> <operator> <num2>")
+        sys.exit(1)
+
+    _, num1, op, num2 = sys.argv
+    try:
+        num1 = float(num1)
+        num2 = float(num2)
+    except ValueError:
+        print("Invalid numbers")
+        sys.exit(1)
+
+    try:
+        if op == "+":
+            result = add(num1, num2)
+        elif op == "-":
+            result = sub(num1, num2)
+        elif op == "*":
+            result = mult(num1, num2)
+        elif op == "/":
+            result = div(num1, num2)
+        else:
+            print("Invalid operator. Use +, -, *, /")
+            sys.exit(1)
+    except ValueError as e:
+        print(str(e))
+        sys.exit(1)
+
+    print(result)
+
+if __name__ == "__main__":
+    main()
